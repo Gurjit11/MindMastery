b4fa4d4b6966500b2d8562ee4d3f092da8e234e7
Gurjit Singh <gurjit9320@gmail.com>
2023-08-06 20:32:11 +0530

    made efficient

--- src/components/HomeQuizes.jsx
+++ src/components/HomeQuizes.jsx
@@ -1,60 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+
+const QuizCategoryCard = ({ title, link, imageSrc }) => {
+  return (
+    <div className="flex justify-between p-1 shadow-2xl max-w-[300px] sm:m-1 rounded-xl">
+      <div className="flex flex-col justify-between">
+        <Link to={link}>
+          <img className="md:min-w-[150px]" src={imageSrc} alt={title} />
+          <div className="text-center font-bold">{title}</div>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 const HomeQuizes = () => {
-    return (
-        <div className='flex justify-center items-center text-white'>
+  const quizCategories = [
+    {
+      title: "Science",
+      link: "/quiz/science",
+      imageSrc: "https://cdn-icons-png.flaticon.com/512/3301/3301569.png",
+    },
+    {
+      title: "Geography",
+      link: "/quiz/geography",
+      imageSrc: "https://cdn-icons-png.flaticon.com/512/4341/4341093.png",
+    },
+    {
+      title: "Maths",
+      link: "/quiz/maths",
+      imageSrc: "https://cdn-icons-png.flaticon.com/512/3153/3153038.png",
+    },
+    {
+      title: "History",
+      link: "/quiz/history",
+      imageSrc: "https://cdn-icons-png.flaticon.com/512/1825/1825357.png",
+    },
+    {
+      title: "World Affairs",
+      link: "/quiz/affairs",
+      imageSrc: "https://cdn-icons-png.flaticon.com/512/8322/8322494.png",
+    },
+  ];
 
-        <div className='flex m-10 gap-10 justify-center items-center bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full sm:w-[70%] px-10'>
-            
+  return (
+    <div className="flex justify-center items-center text-white">
+      <div className="flex m-10 gap-10 justify-center items-center bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full sm:w-[70%] px-10">
+        {quizCategories.map((category, index) => (
+          <QuizCategoryCard key={index} {...category} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-                <div className='flex  justify-between p-1 shadow-2xl max-w-[300px] sm:m-1 rounded-xl'>
-                    <div className='flex flex-col justify-between'>
-
-                        <img className='md:min-w-[150px]' src={'https://cdn-icons-png.flaticon.com/512/3301/3301569.png'} alt='Science' />
-                        <div className='text-center font-bold'><Link to='/quiz/science'>Science</Link></div>
-                    </div>
-                    
-                </div>
-                <div className='flex  justify-between p-1 shadow-2xl max-w-[300px] sm:m-1 rounded-xl'>
-                    <div className='flex flex-col justify-between'>
-
-                        <img className='md:min-w-[150px]' src={'https://cdn-icons-png.flaticon.com/512/4341/4341093.png'} alt='Geography' />
-                        <div className='text-center font-bold'><Link to='/quiz/geography'>Geography</Link></div>
-                    </div>
-                    
-                </div>
-                <div className='flex  justify-between p-1 shadow-2xl max-w-[300px] sm:m-1 rounded-xl'>
-                    <div className='flex flex-col justify-between'>
-
-                        <img className='md:min-w-[150px]' src={'https://cdn-icons-png.flaticon.com/512/3153/3153038.png'} alt='Maths' />
-                        <div className='text-center font-bold'><Link to='/quiz/maths'>Maths</Link></div>
-                    </div>
-                    
-
-                </div>
-            
-
-                <div className='flex  justify-between p-1 shadow-2xl max-w-[300px] sm:m-1 rounded-xl'>
-                    <div className='flex flex-col justify-between'>
-
-                        <img className='md:min-w-[150px]' src={'https://cdn-icons-png.flaticon.com/512/1825/1825357.png'} alt='History' />
-                        <div className='text-center font-bold'><Link to='/quiz/history'>History</Link></div>
-                    </div>
-                    
-                </div>
-                <div className='flex  justify-between p-1 shadow-2xl max-w-[300px] sm:m-1 rounded-xl'>
-                    <div className='flex flex-col justify-between'>
-
-                        <img className='md:min-w-[150px]' src={'https://cdn-icons-png.flaticon.com/512/8322/8322494.png'} alt='World Affairs' />
-                        <div className='text-center font-bold'><Link to='/quiz/affairs'>World Affairs</Link></div>
-                    </div>
-                    
-                </div>
-            
-        </div>
-        </div>
-    )
-}
-
-export default HomeQuizes
+export default HomeQuizes;
--- src/components/HomeSubjects.jsx
+++ src/components/HomeSubjects.jsx
@@ -1,70 +1,68 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SectionCard = ({ title, link, imageSrc, description }) => {
+  return (
+    <div className="flex justify-between p-1 shadow-2xl max-w-[300px] sm:m-1 max-h-[250px] overflow-y-auto rounded-xl border-4 border-blue-500">
+      <div className="flex flex-col justify-between">
+        <Link to={link}>
+          <img className="md:min-w-[150px]" src={imageSrc} alt={title} />
+          <div className="text-center font-bold">{title}</div>
+        </Link>
+      </div>
+      <div className="hidden md:block m-1">{description}</div>
+    </div>
+  );
+};
 
 const HomeSubjects = () => {
-    return (
-        <div className='flex flex-col justify-center items-center'>
-            <div className='flex sm:mx-10 gap-10'>
+  const sectionsData = [
+    {
+      title: "Science",
+      link: "/type/Space science",
+      imageSrc: "https://cdn-icons-png.flaticon.com/512/3301/3301569.png",
+      description:
+        "Science is a methodical approach to understanding the natural world through observation and experimentation.",
+    },
+    {
+      title: "Geography",
+      link: "/type/indian geography",
+      imageSrc: "https://cdn-icons-png.flaticon.com/512/4341/4341093.png",
+      description:
+        "Geography is the study of the Earth's physical features and how people interact with them.",
+    },
+    {
+      title: "Maths",
+      link: "/type/maths icse",
+      imageSrc: "https://cdn-icons-png.flaticon.com/512/3153/3153038.png",
+      description:
+        "Mathematics is the study of numbers, shapes, and their relationships.",
+    },
+    {
+      title: "History",
+      link: "/type/indian history",
+      imageSrc: "https://cdn-icons-png.flaticon.com/512/1825/1825357.png",
+      description:
+        "History is the study of the past using written documents and artifacts to understand how societies and cultures have changed over time.",
+    },
+    {
+      title: "World Affairs",
+      link: "/type/political affairs",
+      imageSrc: "https://cdn-icons-png.flaticon.com/512/8322/8322494.png",
+      description:
+        "World affairs include significant global events such as the COVID-19 pandemic, climate change, conflicts, and struggles for human rights.",
+    },
+  ];
 
-                <div className='flex  justify-between p-1 shadow-2xl max-w-[300px] sm:m-1 rounded-xl border-4 border-blue-500'>
-                    <div className='flex flex-col justify-between'>
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex sm:mx-10 gap-6">
+        {sectionsData.map((section, index) => (
+          <SectionCard key={index} {...section} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-                        <img className='md:min-w-[150px]' src={'https://cdn-icons-png.flaticon.com/512/3301/3301569.png'} alt='Science' />
-                        <div className='text-center font-bold'><Link to='/type/Space science'>Science</Link></div>
-                    </div>
-                    <div className='hidden md:block m-1'>
-                        Science is a methodical approach to understanding the natural world through
-                        observation and experimentation.
-                    </div>
-                </div>
-                <div className='flex  justify-between p-1 shadow-2xl max-w-[300px] sm:m-1 rounded-xl border-4 border-blue-500'>
-                    <div className='flex flex-col justify-between'>
-
-                        <img className='md:min-w-[150px]' src={'https://cdn-icons-png.flaticon.com/512/4341/4341093.png'} alt='Geography' />
-                        <div className='text-center font-bold'><Link to='/type/indian geography'>Geography</Link></div>
-                    </div>
-                    <div className='hidden md:block m-1'>
-                        Geography is the study of the Earth's physical features and how people interact with them.
-                    </div>
-                </div>
-                <div className='flex  justify-between p-1 shadow-2xl max-w-[300px] sm:m-1 rounded-xl border-4 border-blue-500'>
-                    <div className='flex flex-col justify-between'>
-
-                        <img className='md:min-w-[150px]' src={'https://cdn-icons-png.flaticon.com/512/3153/3153038.png'} alt='Maths' />
-                        <div className='text-center font-bold'><Link to='/type/maths icse'>Maths</Link></div>
-                    </div>
-                    <div className='hidden md:block m-1'>
-                        Mathematics is the study of numbers, shapes, and their relationships.
-
-                    </div>
-
-                </div>
-            </div>
-            <div className='flex mx-10 gap-10'>
-
-                <div className='flex  justify-between p-1 shadow-2xl max-w-[300px] sm:m-1 rounded-xl border-4 border-blue-500'>
-                    <div className='flex flex-col justify-between'>
-
-                        <img className='md:min-w-[150px]' src={'https://cdn-icons-png.flaticon.com/512/1825/1825357.png'} alt='History' />
-                        <div className='text-center font-bold'><Link to='/type/indian history'>History</Link></div>
-                    </div>
-                    <div className='hidden md:block m-1'>
-                        History is the study of the past using written documents and artifacts to understand how societies and cultures have changed over time.
-                    </div>
-                </div>
-                <div className='flex  justify-between p-1 shadow-2xl max-w-[300px] sm:m-1 rounded-xl border-4 border-blue-500'>
-                    <div className='flex flex-col justify-between'>
-
-                        <img className='md:min-w-[150px]' src={'https://cdn-icons-png.flaticon.com/512/8322/8322494.png'} alt='World Affairs' />
-                        <div className='text-center font-bold'><Link to='/type/political affairs'>World Affairs</Link></div>
-                    </div>
-                    <div className='hidden md:block m-1'>
-                        World affairs include significant global events such as the COVID-19 pandemic, climate change, conflicts, and struggles for human rights.
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default HomeSubjects
+export default HomeSubjects;
