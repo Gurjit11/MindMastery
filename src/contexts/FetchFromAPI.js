d3b5e62e3a27d5d777fe34734502aed8bd59ea34
Gurjit Singh <gurjit9320@gmail.com>
2023-08-06 20:38:34 +0530

    made efficient

--- src/contexts/FetchFromAPI.js
+++ src/contexts/FetchFromAPI.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
-const base_url ='https://youtube-v31.p.rapidapi.com'
+const base_url = 'https://youtube-v31.p.rapidapi.com'
 
 const options = {
-    params: {
-        // part: 'snippet', 
-        // videoId: 'M7FIvfx5J10'
-        maxResults:"12"
-    },
-    headers: {
-      'X-RapidAPI-Key': your key here ,
-      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-    }
-  };
+  params: {
+    // part: 'snippet', 
+    // videoId: 'M7FIvfx5J10'
+    maxResults: "12"
+  },
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_VIDEO_API,
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
 
 export const FetchFromAPI = async (url) => {
-   const {data} = await axios.get(`${base_url}/${url}`,options);
-   return data
+  const { data } = await axios.get(`${base_url}/${url}`, options);
+  return data
 }
--- src/pages/Typevideos.jsx
+++ src/pages/Typevideos.jsx
@@ -1,42 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import VideoCard from '../components/VideoCard'
-import { FetchFromAPI } from '../contexts/FetchFromAPI'
-
-import { MdChevronRight, MdChevronLeft } from 'react-icons/md'
+import React, { useEffect, useState } from "react";
+import VideoCard from "../components/VideoCard";
+import { FetchFromAPI } from "../contexts/FetchFromAPI";
 
 const Typevideos = ({ type }) => {
-    const [videos, setVideos] = useState([])
+  const [videos, setVideos] = useState([]);
 
-    useEffect(() => {
-        FetchFromAPI(`search?part=snippet&q=${type}`)
-            .then((data) => setVideos(data.items))
-    }, [type])
+  useEffect(() => {
+    FetchFromAPI(`search?part=snippet&q=${type}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [type]);
 
-    const slideLeft = () => {
-        var slider = document.getElementById('slider' )
-        slider.scrollLeft = slider.scrollLeft - 500;
-    }
-    const slideRight = () => {
-        var slider = document.getElementById('slider')
-        slider.scrollLeft = slider.scrollLeft + 500;
-    }
-     console.log(videos)
+  console.log(videos);
 
-    return (
-     
-        <div className='relative flex items-center group justify-center m-10'>
-           <div id={`slider`} className='gap-3 grid lg:grid-cols-4 w-full'>
+  return (
+    <div className="relative flex items-center group justify-center m-10">
+      <div id={`slider`} className="gap-3 grid lg:grid-cols-4 w-full">
+        {videos.map((item, id) => (
+          <div key={id}>
+            {item.id.videoId && <VideoCard video={item} />}
+            {/* {item.id.channelId && {}} */}
+            {console.log(item)}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-        { videos.map((item,id) => (
-                <div key={id}>
-                    {item.id.videoId &&<VideoCard  video={item} />}
-                    {/* {item.id.channelId && {}} */}
-                    {console.log(item)}
-                </div>
-            ))}
-        </div>
-       </div>
-    )
-}
-
-export default Typevideos
+export default Typevideos;
